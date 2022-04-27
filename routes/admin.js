ba9ff97771db13fd1e67f9ebeafc16c3ace35f3f
hoda411 <71883856+hoda411@users.noreply.github.com>
2022-04-27 22:56:14 +0200

    Route For First Entity (Admin)

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
-router.get('/users', (req,res,next)=>{
-    
-res.status(200).json({data:"hello"})
 
+// Show All Users
+router.get('/admin/users', (req,res,next)=>{
+    
+res.status(200).json({data:"Get-All-Users"})
+
+});
+
+// Show All Courses Details
+router.get('/admin/courses/details', (req,res,next)=>{
+    
+    res.status(200).json({data:"Get-All-Courses-Details"})
+    
+});
+
+// Add New User 
+router.post('/admin/add-user', (req,res,next)=>{
+    
+    res.status(200).json({data:"User Is Added"})
+    
+});
+
+// Delete An Existing User
+router.delete('/admin/delete-user',(req,res,next)=>{
+res.status(200).json({data:"User Is Deleted "})
 });
 module.exports = router;
